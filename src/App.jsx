ba30b48d1ec157f69261dc7daba855a45abc51c0
sega <sergohack2015@gmail.com>
2023-03-23 22:59:33 +0200

    transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.scss';
 import Dashboard from './pages/Dashboard';
 import Appointment from './pages/Appointment';
+import Transaction from './pages/Transaction';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Appointment />
+      {/* <Dashboard />
+      <Appointment /> */}
+      <Transaction />
     </div>
   )
 }
---
+++ src/pages/Transaction.jsx
@@ -0,0 +1,85 @@
+import usersIcon from '../assets/svg/Users.svg'
+import percent from '../assets/svg/Discount.svg'
+import activity from '../assets/svg/Activity.svg'
+
+let Transaction = () => {
+    return (
+        <div className="transaction__container container">
+            <span className="transaction__title headline-900">Transaction</span>
+
+            <div className="transaction__total section">
+                <div className="total__header">
+                    <img className="total__icon" src={usersIcon} alt="svg" />
+                    <div className="total__header-right">
+                        <span className="total__percent">+8.12%</span>
+                        <span className='total__lastyear'>from last year</span>
+                    </div>
+                </div>
+
+                <div className="total__footer">
+                    <span className="total__number headline-800">1192</span>
+                    <span className="total__title">Total patient</span>
+                </div>
+
+            </div>
+
+
+            <div className="transaction__total section">
+                <div className="total__header">
+                    <img className="total__icon" src={usersIcon} alt="svg" />
+                    <div className="total__header-right">
+                        <span className="total__percent">+8.12%</span>
+                        <span className='total__lastyear'>from last year</span>
+                    </div>
+                </div>
+
+                <div className="total__footer">
+                    <span className="total__number headline-800">1192</span>
+                    <span className="total__title">Total patient</span>
+                </div>
+
+            </div>
+
+
+            <div className="transaction__total section">
+                <div className="total__header">
+                    <img className="total__icon" src={usersIcon} alt="svg" />
+                    <div className="total__header-right">
+                        <span className="total__percent">+8.12%</span>
+                        <span className='total__lastyear'>from last year</span>
+                    </div>
+                </div>
+
+                <div className="total__footer">
+                    <span className="total__number headline-800">1192</span>
+                    <span className="total__title">Total patient</span>
+                </div>
+
+            </div>
+
+
+            <div className="transaction__total section">
+                <div className="total__header">
+                    <img className="total__icon" src={usersIcon} alt="svg" />
+                    <div className="total__header-right">
+                        <span className="total__percent">+8.12%</span>
+                        <span className='total__lastyear'>from last year</span>
+                    </div>
+                </div>
+
+                <div className="total__footer">
+                    <span className="total__number headline-800">1192</span>
+                    <span className="total__title">Total patient</span>
+                </div>
+
+            </div>
+
+            <div className="transaction__total"></div>
+            <div className="transaction__total"></div>
+            <div className="transaction__total"></div>
+
+        </div>
+    )
+}
+
+export default Transaction;
