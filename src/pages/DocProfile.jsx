e8dc8cd088bcd7779c16f6fb2bfb9861dcf653d9
sega <sergohack2015@gmail.com>
2023-03-24 16:51:28 +0200

    test doc

--- src/pages/DocProfile.jsx
+++ src/pages/DocProfile.jsx
@@ -3,14 +3,20 @@
 let DocProfile = () => {
     return (
         <div className="docprofile__container container">
-            <div className="section">
-                <section id="section1"></section>
-                <img class="avatar-container"src="https://randomuser.me/api/portraits/women/1.jpg" alt="Avatar"></img>
-    
-
-                
+            <div className="docprofile__photo">
+                <img src="https://randomuser.me/api/portraits/women/3.jpg" width='90' alt="" />
             </div>
+
+            <div className="docprofile__header section">
+
+
+                <span>dr. Selena odette</span>
+                <span>Specialist of skin surgery</span>
+                <div className="docprofile__location"></div>
+            </div>
+
         </div>
+
     )
 }
 
